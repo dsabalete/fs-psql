e3672dbc4a446abd4678dfe2649a47cd71a78c6e
David <dsabalete@gmail.com>
2026-04-19 18:54:26 +0200

    Revisiting many-to-many relationships

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,23 @@
 const router = require('express').Router()
+
 const { User, Note, Team } = require('../models')
 const { tokenExtractor, isAdmin } = require('../util/middleware')
+
+router.put('/:username', tokenExtractor, isAdmin, async (req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: req.params.username
+        }
+    })
+
+    if (user) {
+        user.disabled = req.body.disabled
+        await user.save()
+        res.json(user)
+    } else {
+        res.status(404).end()
+    }
+})
 
 router.get('/', async (req, res) => {
     const users = await User.findAll({
@@ -8,6 +25,18 @@ router.get('/', async (req, res) => {
             {
                 model: Note,
                 attributes: { exclude: ['userId'] }
+            },
+            {
+                model: Note,
+                as: 'marked_notes',
+                attributes: { exclude: ['userId'] },
+                through: {
+                    attributes: []
+                },
+                include: {
+                    model: User,
+                    attributes: ['name']
+                }
             },
             {
                 model: Team,
@@ -31,35 +60,8 @@ router.post('/', async (req, res) => {
 })
 
 router.get('/:id', async (req, res) => {
-    const user = await User.findByPk(req.params.id, {
-        include: [
-            {
-                model: Note,
-            }
-        ]
-    })
-
+    const user = await User.findByPk(req.params.id)
     if (user) {
-        res.json({
-            username: user.username,
-            name: user.name,
-            note_count: user.notes.length
-        })
-    } else {
-        res.status(404).end()
-    }
-})
-
-router.put('/:username', tokenExtractor, isAdmin, async (req, res) => {
-    const user = await User.findOne({
-        where: {
-            username: req.params.username
-        }
-    })
-
-    if (user) {
-        user.disabled = req.body.disabled
-        await user.save()
         res.json(user)
     } else {
         res.status(404).end()
